a783472f63b49da0d9a06fee96a650332ce3ec80
Fedesa Yelmachew <fedhasayelmachew@gmail.com>
2024-11-29 10:20:35 +0300

    Done auth controller

--- Job Bord/backend/controller/authController/authController.js
+++ Job Bord/backend/controller/authController/authController.js
@@ -1,14 +1,62 @@
+const db = require('../../database/database')
+const statCodes = require('http-status-codes')
+const bcrypt = require("bcrypt")
+const jwt = require('jsonwebtoken')
 
+const register = async (req, res) => {
+    const { fname, lname, email, role, password } = req.body
 
-const register = async(req, res) => {
-    res.send('register')
-    
+    if (!fname || !lname || !email || !role || !password) {
+        return res.status(statCodes.BAD_REQUEST).json({ msg: "Incomplete information" })
+    }
+    if (password.length < 6) {
+        return res.status(statCodes.BAD_REQUEST).json({ msg: "Password must not be less than 6 characters" })
+    }
+
+    try {
+        const [user] = await db.query("SELECT user_id FROM users WHERE email=?", [email])
+        if (user.length > 0) {
+            return res.status(statCodes.BAD_REQUEST).json({ msg: "Email registed already" })
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+
+        const result = await db.query("INSERT INTO users (fname, lname, email, role, password) VALUES (?,?,?,?,?)", [fname, lname, email, role, hash])
+        res.status(statCodes.OK).json({msg: "Registered successfully"})
+
+    } catch (error) {
+        res.status(statCodes.INTERNAL_SERVER_ERROR).json({ msg: "Something went wrong, please try again." })
+        console.log(error)
+    }
 }
 
-const login = async(req, res) => {
-    res.send('login')
+const login = async (req, res) => {
+    const {email, password} = req.body
+    if (!email || !password){
+        return res.status(statCodes.BAD_REQUEST).json({msg: "Incomplete information"})
+    }
 
+    try {
+        const [user] = await db.query("SELECT user_id, fname, password FROM users WHERE email=?", [email])
+        if (user.length !== 1){
+            return res.status(statCodes.UNAUTHORIZED).json({msg: "Invalid credentials"})
+        }
+        const isMatch = await bcrypt.compare(password, user[0].password)
+        if (!isMatch){
+            return res.status(statCodes.UNAUTHORIZED).json({msg: "Invalid credentials"})
+        }
+
+        const fname = user[0].fname
+        const userId = user[0].user_id
+        const token = jwt.sign({fname, userId}, process.env.JWT_SECRET, {expiresIn: '1d'}) 
+        res.status(statCodes.OK).json({msg: "Logged in", token})
+
+    } catch (error) {
+        console.log(error)
+        return res.status(statCodes.INTERNAL_SERVER_ERROR).json({msg:"Something went wrong, please try again."})
+    }
 }
 
 
-module.exports = {register, login}
+module.exports = { register, login }
