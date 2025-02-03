c27c64c094b45268595a0209cf03fd680a6c77e2
Fedesa Yelmachew <fedhasayelmachew@gmail.com>
2025-02-03 22:56:38 +0300

    Landing page hero section done

--- Job Bord/frontend/job-board/src/components/footer/Footer.jsx
+++ Job Bord/frontend/job-board/src/components/footer/Footer.jsx
@@ -7,15 +7,15 @@ function Footer() {
 
   return (
     <footer className='flex justify-around p-2 items-center bg-gray-800 text-white xl:text-xl '>
-      <div className={` flex flex-col  items-left text-white `}>
+      <div className={` flex flex-col mb-2 gap-1 items-left text-white `}>
         <p>Fedesa Yelmachew</p>
-        <a className={` items-center hover:text-white
+        <a className={` flex items-center gap-1 hover:text-white
                   hover:scale-110 transition-transform duration-300 ease-out`} href="mailto:fedhasayelmachew@gmail.com"><> < EmailIcon /> Email me </></a>
       </div>
-      <div className={` flex flex-col  items-left text-white `}>
-        <a className={` items-center hover:text-white
+      <div className={` flex flex-col mb-2 gap-1 items-left text-white `}>
+        <a className={` flex items-center gap-1 hover:text-white
                   hover:scale-125 transition-transform duration-300 ease-out`} href="https://www.linkedin.com/in/linkfedhako7/" target="_blank" rel="noopener noreferrer" > <> < LinkedInIcon /> LinkedIn </> </a>
-        <a className={` items-center hover:text-white
+        <a className={` flex items-center gap-1 hover:text-white
                   hover:scale-110 transition-transform duration-300 ease-out`} href="https://github.com/fedhako7?tab=repositories" target="_blank" rel="noopener noreferrer"> <> < GitHubIcon /> gitHub </> </a>
       </div>
     </footer>
--- Job Bord/frontend/job-board/src/pages/landing/Landing.jsx
+++ Job Bord/frontend/job-board/src/pages/landing/Landing.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import hero_image from '../../assets/hero_image.webp'
 
 function Landing() {
   return (
-    <div>Landing</div>
+    <section className={` bg-[#8A9A5B] w-full `}>
+      {/* Hero Section */}
+      <section className={` flex flex-col justify-around items-center md:flex-row `}>
+
+        <div className={` flex-grow m-4 p-4`}>
+          <h1 className='flex flex-grow text-7xl mb-3 xl:mb-5 '>Find your dream job!</h1>
+          <p className=' flex flex-grow text-lg xl:text-2xl'>
+            Search for your dream job and apply with one click!
+          </p>
+          {/* Search Input field here */}
+        </div>
+
+        <div className="flex relative">
+          <img
+            src={hero_image}
+            className="w-[84%] max-w-[950px] m-4 pl-4 self-center object-cover"
+          />
+          <div className="w-[84%] m-4 mb-2 pl-4 absolute inset-0 bg-gradient-to-b from-transparent to-[#f8f9fa] to-95%">
+          </div>
+        </div>
+      </section>
+
+      {/* How it works */}
+      <section>
+        <div>
+          How It Works
+        </div>
+      </section>
+
+      {/* About Section */}
+      <section className='flex flex-col justify-center items-center'>
+        <div>
+          <h1>About Job Bord</h1>
+        </div>
+      </section>
+
+      {/* Contact Section */}
+      <section className='flex flex-col justify-center items-center'>
+        <div>
+          <h1>Contact</h1>
+        </div>
+      </section>
+
+    </section>
   )
 }
 
