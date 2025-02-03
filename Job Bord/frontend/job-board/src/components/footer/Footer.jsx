84f1c585af08102aec9e4cefd31febe8339ea112
Fedesa Yelmachew <fedhasayelmachew@gmail.com>
2025-02-03 11:06:18 +0300

    Updated Footer

--- Job Bord/frontend/job-board/src/components/footer/Footer.jsx
+++ Job Bord/frontend/job-board/src/components/footer/Footer.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import SearchIcon from '@mui/icons-material/Search';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
 
 function Footer() {
 
   return (
-    <footer className='flex h-36 bg-gray-800 '>
-
-      <div>
-        <p className='text-white text-left text-2xl pt-6 pl-12 font-medium '>Built by: Fedesa Yelmachew</p>
+    <footer className='flex justify-around p-2 items-center bg-gray-800 text-white xl:text-xl '>
+      <div className={` flex flex-col  items-left text-white `}>
+        <p>Fedesa Yelmachew</p>
+        <a className={` items-center hover:text-white
+                  hover:scale-110 transition-transform duration-300 ease-out`} href="mailto:fedhasayelmachew@gmail.com"><> < EmailIcon /> Email me </></a>
       </div>
-
+      <div className={` flex flex-col  items-left text-white `}>
+        <a className={` items-center hover:text-white
+                  hover:scale-125 transition-transform duration-300 ease-out`} href="https://www.linkedin.com/in/linkfedhako7/" target="_blank" rel="noopener noreferrer" > <> < LinkedInIcon /> LinkedIn </> </a>
+        <a className={` items-center hover:text-white
+                  hover:scale-110 transition-transform duration-300 ease-out`} href="https://github.com/fedhako7?tab=repositories" target="_blank" rel="noopener noreferrer"> <> < GitHubIcon /> gitHub </> </a>
+      </div>
     </footer>
   )
 }
