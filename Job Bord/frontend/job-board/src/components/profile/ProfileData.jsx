917b7acaaae8103cd2262aab6251468e161da458
Fedesa Yelmachew <fedhasayelmachew@gmail.com>
2025-03-03 10:34:17 +0300

    set up environment for remote db and profile margins

--- Job Bord/frontend/job-board/src/components/profile/ProfileData.jsx
+++ Job Bord/frontend/job-board/src/components/profile/ProfileData.jsx
@@ -33,7 +33,7 @@ const ProfileData = () => {
   }, [child])
   return (
     <section className="flex w-full flex-grow ">
-      <div className="flex flex-col w-5/6 bg-white ml-auto mr-auto mt-4 gap-4 p-5 border-2 border-gray-400">
+      <div className="flex flex-col w-5/6 bg-white ml-auto mr-auto mt-4 mb-6 gap-4 p-5 border-2 border-gray-400">
 
         <div className='flex flex-col'>
           <label className="text-lg text-gray-800 font-semibold">First name</label>
